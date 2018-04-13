65a7b31ff6f7e4a6f2608cef2ff106d401b8502f
Gabriel Nascimento <gabriel.mfn@gmail.com>
2018-04-13 15:40:13 -0300

    Adicionado o formRequest SavePersona

---
+++ app/Http/Requests/SavePersona.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SavePersona extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nome' =>'required|min:2|max:25',
+            'np' =>'required|integer',
+            'forca' =>'required|integer',
+            'destreza' =>'required|integer',
+            'constituicao' =>'required|integer',
+            'inteligencia' =>'required|integer',
+            'sabedoria' =>'required|integer',
+            'carisma' =>'required|integer',
+            'dano' =>'required|integer',
+            'ataque' =>'required|integer',
+            'defesa' =>'required|integer',
+            'vida' =>'required|integer',
+            'iniciativa' =>'required|integer',
+            'resistencia' =>'required|integer',
+            'reflexo' =>'required|integer',
+            'fortitude' =>'required|integer',
+            'vontade' =>'required|integer',
+        ];
+    }
+}
